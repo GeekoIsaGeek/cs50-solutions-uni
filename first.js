8c3284d225a8975528b3a1ea340738b9664799d5
geekoisageek <george.kvrivi@gmail.com>
2024-11-13 21:52:20 +0400

    chore: add pseudo code

--- first.js
+++ first.js
@@ -1,18 +1,29 @@
-const hats = ['F', 'B', 'B', 'F', 'F', 'B', 'F'];
+const initialState = ['F', 'B', 'B', 'F', 'F', 'B', 'F'];
 
-const hatsCount = {
+const hats = {
 	F: 0,
 	B: 0,
 };
 
-hats.forEach((hat) => {
+initialState.forEach((hat) => {
 	if (hat === 'F') {
-		hatsCount.F++;
+		hats.F++;
 	} else {
-		hatsCount.B++;
+		hats.B++;
 	}
 });
 
-const result = hatsCount.F > hatsCount.B ? hats.map((hat) => 'F') : hats.map((hat) => 'B');
+const result = hats.F > hats.B ? initialState.map((hat) => 'F') : initialState.map((hat) => 'B');
+const minNumOfSwitches = Math.min(...Object.values(hats));
 
-console.log(result);
+console.log(
+	`There were only ${minNumOfSwitches} switches needed to make everyone wear their hats the same way!`,
+	`\n${result.join(', ')}`
+);
+
+// Pseudo code:
+// გვაქვს F-სგან და B-სგან შევსებული მასივი
+// დაგვჭირდება დავითვალოთ რაოდენობრივად რამდენია თითოეული ელემენტი
+// შემდეგ მასივის ყოველ ელემენტს შევამოწმებთ და თუ F ელემენტი შეგვხვდება მაშინ ვზრდით F-ის რაოდენობას, same for B
+// მასივის ყველა ელემენტის შემოწმების მერე შევადარებთ ამ ელემენტების რაოდენობას
+// რომელი ელემენტიც მეტი იქნება, ამ ელემენტით შევცვლით ყველა განსხვავებულს if F > B: B->F, else F->B
